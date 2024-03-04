95e19116ee674f0def473fbdde316dbfbe0077dd
jfsenechal <jfsenechal@gmail.com>
2024-03-04 16:45:58 +0100

    add TransitionGroup

--- components/ListResult.vue
+++ components/ListResult.vue
@@ -13,13 +13,24 @@ function imageFiche(fiche) {
   return 'https://picsum.photos/1280'
 }
 </script>
+<style>
+.list-enter-active,
+.list-leave-active {
+  @apply transition-all duration-700 ease-in-out  transform translate-x-0 opacity-100
+}
+.list-enter-from,
+.list-leave-to {
+  @apply opacity-0 translate-x-full
+}
+</style>
 <template>
   <section class="relative h-full overflow-hidden rounded-xl border border-dashed border-gray-400 opacity-100"
-       id="list-result" :data-markers="data.hits">
+           id="list-result" :data-markers="data.hits">
     <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-8 lg:max-w-full lg:px-8">
-      <div class="grid grid-cols-1 gap-y-4 lg:grid-cols-3 sm:gap-x-6 sm:gap-y-4 lg:gap-x-4">
-        <div class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white45"
-             v-for="fiche in data.hits" :key="fiche.id">
+      <TransitionGroup name="list" tag="ul"
+                       class="grid grid-cols-1 gap-y-4 lg:grid-cols-3 sm:gap-x-6 sm:gap-y-4 lg:gap-x-4">
+        <li class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white45"
+            v-for="fiche in data.hits" :key="fiche.id">
           <div class="aspect-square bg-gray-200 sm:aspect-none group-hover:opacity-75 h-80 sm:h-96">
             <img :src="imageFiche(fiche)"
                  :alt="fiche.societe"
@@ -40,8 +51,8 @@ function imageFiche(fiche) {
               <p class="text-sm italic text-gray-500">{{ fiche.telephone }}</p>
             </div>
           </div>
-        </div>
-      </div>
+        </li>
+      </TransitionGroup>
     </div>
   </section>
 </template>
