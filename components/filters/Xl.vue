4460fd685140714761f5998fe76c9855f6f8b472
jfsenechal <jfsenechal@gmail.com>
2024-03-04 16:29:53 +0100

    add root aside

--- components/filters/Xl.vue
+++ components/filters/Xl.vue
@@ -11,7 +11,7 @@ defineProps({
 const menuOpen = defineModel('menuOpen')
 
 function manageFilters2(name, value, event) {
-   manageFilters(filters,name,value,event)
+  manageFilters(filters, name, value, event)
 }
 
 function isChecked(name, value) {
@@ -27,45 +27,47 @@ function isChecked(name, value) {
 }
 </script>
 <template>
-  <h2 class="sr-only">Filtres</h2>
-  <!-- Mobile filter dialog toggle, controls the 'mobileFilterDialogOpen' state. -->
-  <button type="button" class="inline-flex items-center px-4 sm:px-0 lg:hidden" @click="menuOpen = true">
-    <IconFilter/>
-    <span class="text-sm font-medium text-carto-main">Filtres</span>
-    <svg class="ml-1 h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
-         aria-hidden="true">
-      <path
-          d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"/>
-    </svg>
-  </button>
+  <aside>
+    <h2 class="sr-only">Filtres</h2>
+    <!-- Mobile filter dialog toggle, controls the 'mobileFilterDialogOpen' state. -->
+    <button type="button" class="inline-flex items-center px-4 sm:px-0 lg:hidden" @click="menuOpen = true">
+      <IconFilter/>
+      <span class="text-sm font-medium text-carto-main">Filtres</span>
+      <svg class="ml-1 h-5 w-5 flex-shrink-0 text-gray-400" viewBox="0 0 20 20" fill="currentColor"
+           aria-hidden="true">
+        <path
+            d="M10.75 4.75a.75.75 0 00-1.5 0v4.5h-4.5a.75.75 0 000 1.5h4.5v4.5a.75.75 0 001.5 0v-4.5h4.5a.75.75 0 000-1.5h-4.5v-4.5z"/>
+      </svg>
+    </button>
 
-  <div class="hidden lg:block lg:min-w-36">
-    <form class="space-y-10 divide-y divide-gray-200">
-      <template v-for="(item,facetName,index) in data.facetDistribution" :key="index">
-        <div class="[&:not(:first-child)]:pt-10" v-if="nameStartsWithUnderscore(facetName)">
-          <fieldset>
-            <legend class="block text-sm font-medium text-gray-900">
-              {{ capitalized(facetName) }}
-            </legend>
-            <div class="space-y-3 pt-6">
-              <div v-for="(nb,name) in item" class="flex items-center" :key="item.name">
-                <input :id="`filter-${name}`" :name="`${facetName}[]`" :value="name"
-                       :checked="isChecked(facetName,name)"
-                       @change="manageFilters2(facetName,name,$event)"
-                       type="checkbox"
-                       class="h-4 w-4 rounded border-carto-gray200 text-carto-pink focus:ring-carto-pink"
-                >
-                <label :for="`filter-${name}`"
-                       class="ml-3 text-sm text-carto-main">
-                  <i :class="data.icons[name]['icon']"
-                     :style="`font-size: 1rem;color:${data.icons[name]['color']}`"></i>
-                  {{ name }} ({{ nb }})
-                </label>
+    <div class="hidden lg:block lg:min-w-36">
+      <form class="space-y-10 divide-y divide-gray-200">
+        <template v-for="(item,facetName,index) in data.facetDistribution" :key="index">
+          <div class="[&:not(:first-child)]:pt-10" v-if="nameStartsWithUnderscore(facetName)">
+            <fieldset>
+              <legend class="block text-sm font-medium text-gray-900">
+                {{ capitalized(facetName) }}
+              </legend>
+              <div class="space-y-3 pt-6">
+                <div v-for="(nb,name) in item" class="flex items-center" :key="item.name">
+                  <input :id="`filter-${name}`" :name="`${facetName}[]`" :value="name"
+                         :checked="isChecked(facetName,name)"
+                         @change="manageFilters2(facetName,name,$event)"
+                         type="checkbox"
+                         class="h-4 w-4 rounded border-carto-gray200 text-carto-pink focus:ring-carto-pink"
+                  >
+                  <label :for="`filter-${name}`"
+                         class="ml-3 text-sm text-carto-main">
+                    <i :class="data.icons[name]['icon']"
+                       :style="`font-size: 1rem;color:${data.icons[name]['color']}`"></i>
+                    {{ name }} ({{ nb }})
+                  </label>
+                </div>
               </div>
-            </div>
-          </fieldset>
-        </div>
-      </template>
-    </form>
-  </div>
+            </fieldset>
+          </div>
+        </template>
+      </form>
+    </div>
+  </aside>
 </template>
