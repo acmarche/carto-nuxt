f38680f196f620a0646dd20d92dc98db096f2e4b
jfsenechal <jfsenechal@gmail.com>
2024-03-04 08:54:32 +0100

    add

---
+++ components/widgets/SaveCss.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="transition ease-in-out duration-700 transform opacity-0  transition ease-in-out duration-300 transform translate-x-0">
+
+  </div>
+  <div class="transition ease-in-out duration-300 transform translate-x-full opacity-100 ">
+
+  </div>
+</template>
