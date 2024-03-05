6f1d51eccf29dbe3ef640beaa02af1b526ddaf03
jfsenechal <jfsenechal@gmail.com>
2024-03-05 11:47:59 +0100

    change typo

--- components/Main.vue
+++ components/Main.vue
@@ -42,14 +42,11 @@ watch(() => propos.coords, (newValue, oldValue) => {
         bg-[length:250px_100%] bg-no-repeat
         px-10 py-1 text-center text-4xl font-bold
         animate-shimmer">
-          Du bio, du local, du solidaire...
+          Du local, du circuit-cour, du solidaire...
         </h1>
-        <h1 class="text-4xl font-bold lobster-two-bold tracking-tight text-carto-pink">Circuit-Court</h1>
+        <h1 class="text-4xl font-bold lobster-two-bold tracking-tight text-carto-pink">Carte dynamique</h1>
         <p class="mt-4 text-2xl text-carto-main lobster-two-regular-italic">
-          On qualifie généralement de circuit court ou circuit de commercialisation le circuit de
-          distribution
-          dans lequel
-          intervient au maximum un intermédiaire entre le producteur et le consommateur.
+          Vous trouverez sur cette carte les acteurs et intervenants de différentes filières liées au circuit court dans la commune de Marche-en-Famenne.
         </p>
         <p class="text-sm text-carto-gray200 m-2">
           Votre sélection {{ filters }}
