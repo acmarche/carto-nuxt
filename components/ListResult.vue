c94ff160ee1e0903adb4191fbf3c952e4d1495b9
jfsenechal <jfsenechal@gmail.com>
2024-03-08 13:09:16 +0100

    padding down

--- components/ListResult.vue
+++ components/ListResult.vue
@@ -20,7 +20,7 @@ defineProps({
 </style>
 <template>
   <section class="relative h-full overflow-hidden rounded-xl border border-dashed border-gray-400 opacity-100">
-    <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-8 lg:max-w-full lg:px-8">
+    <div class="mx-auto max-w-2xl px-4 py-4 sm:px-4 sm:py-4 lg:max-w-full lg:px-4">
       <TransitionGroup name="list" tag="ul"
                        class="grid grid-cols-1 gap-y-4 lg:grid-cols-3 sm:gap-x-6 sm:gap-y-4 lg:gap-x-4">
         <li class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white45"
