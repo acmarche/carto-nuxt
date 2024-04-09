b62874e8cfcb2a009bb6e8f982b4dec7d5aea990
jfsenechal <jfsenechal@gmail.com>
2024-04-09 20:09:44 +0200

    debug

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     devtools: {enabled: true},
-    debug: false,
+    debug: true,
     modules: ['@nuxtjs/tailwindcss', '@nuxtjs/google-fonts'],
     plugins: [
         {src: '~/plugins/leaflet.ts', mode: 'client'}
