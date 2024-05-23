d5937a102bcd72f45bb0a9bb204741e028678ff0
jfsenechal <jfsenechal@gmail.com>
2024-05-23 14:17:48 +0200

    replace cat

--- components/shop/Secteur.vue
+++ components/shop/Secteur.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 const config = useRuntimeConfig()
-import {IconCat} from '@tabler/icons-vue';
+import {IconTag} from '@tabler/icons-vue';
 
 const {shop} = defineProps<{
   shop: { type: Object, required: true },
@@ -14,14 +14,14 @@ const {shop} = defineProps<{
         <img :src="`${config.public.BOTTIN_URL}/bottin/icons/${secteur.icon}`" v-if="secteur.icon" alt="icon"
              class="h-8 w-8"
              aria-hidden="true"/>
-        <IconCat class="h-8 w-8" aria-hidden="true" v-else/>
+        <IconTag class="h-8 w-8" aria-hidden="true" v-else/>
         <span>{{ secteur.name }}</span>
       </li>
     </ul>
     <ul class="mt-2">
       <li v-for="tag in shop.tagsObject" class="flex flex-row items-center gap-2" :key="tag.id">
         <img :src="`${config.public.BOTTIN_URL}/bottin/tags/${tag.icon}`" alt="icon" class="w-8 h-8" v-if="tag.icon"/>
-        <IconCat class="h-8 w-8" aria-hidden="true" v-else/>
+        <IconTag class="h-8 w-8" aria-hidden="true" v-else/>
         <span>{{ tag.name }}</span>
       </li>
     </ul>
