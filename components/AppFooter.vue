78234ee764108276de2175c52ee959336f66f0a5
jfsenechal <jfsenechal@gmail.com>
2024-08-01 14:12:12 +0200

    up

--- components/AppFooter.vue
+++ components/AppFooter.vue
@@ -1,5 +1,34 @@
+<script setup lang="ts">
+</script>
 <template>
-  <footer class="bg-carto-gray100 h-16 w-full flex items-center justify-center" aria-labelledby="footer-heading">
-    <h2 id="footer-heading" class="">Administration communale de Marche-en-Famenne</h2>
+  <footer class="bg-carto-gray200">
+    <div class="mx-auto max-w-7xl overflow-hidden px-6 py-4 sm:py-12 lg:px-8">
+      <nav class="-mb-6 columns-2 sm:flex sm:justify-center sm:space-x-12" aria-label="Footer">
+        <div class="pb-6">
+          <NuxtLink to="/" class="leading-6 text-white hover:text-">Carte</NuxtLink>
+        </div>
+        <div class="pb-6">
+          <NuxtLink to="/acteur" class="leading-6 text-white">Liste</NuxtLink>
+        </div>
+        <div class="pb-6">
+          <NuxtLink to="/geolocalisation" class="leading-6 text-white">Autour de moi
+          </NuxtLink>
+        </div>
+        <div class="pb-6">
+          <NuxtLink to="/about" class="leading-6 text-white ">A propos de</NuxtLink>
+        </div>
+        <div class="pb-6">
+          <NuxtLink to="/filiere" class="leading-6 text-white">Par filière</NuxtLink>
+        </div>
+        <div class="pb-6">
+          <NuxtLink to="/localite" class="leading-6 text-white ">Par localité</NuxtLink>
+        </div>
+      </nav>
+      <p class="mt-10 text-center leading-5">
+        <NuxtLink to="https://www.marche.be/administration/rgpd">
+          &copy; 2024 Marche-en-Famanne, Administration communale. Tous droits réservés.
+        </NuxtLink>
+      </p>
+    </div>
   </footer>
 </template>
