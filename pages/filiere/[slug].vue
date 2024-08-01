3225f24dbf8425fa091584ab2c6d4bc119298963
jfsenechal <jfsenechal@gmail.com>
2024-08-01 16:10:08 +0200

    bof

--- pages/filiere/[slug].vue
+++ pages/filiere/[slug].vue
@@ -1,28 +1,21 @@
 <script setup>
+const config = useRuntimeConfig()
 const breadcrumb = [
   {name: "Retour à la liste", link: "/filiere", id: 1}
 ]
 const slug = useRoute().params.slug
-const items = ref([])
 const {tag} = tagGet(slug)
-
-function execute() {
-  const filters = ref({localite: null, tags: [tag?.value?.name], coordinates: null})
-  const {
-    status,
-    data,
-    error
-  } = updateSearchGet(filters)
-  if (data.value?.hits?.length > 0) {
-    items.value = data?.value?.hits ?? []
-  }
-}
-watch(tag, (val) => {
+const filters = ref({localite: null, tags: [tag.value?.name], coordinates: null})
+const {
+  status,
+  data,
+  error: errorSearch
+} = updateSearchGet(filters)
+watch(tag, async (val) => {
   if (val) {
-    console.log('coucou')
-    execute()
+    filters.value = {localite: null, tags: [tag.value.name], coordinates: null}
   }
-}, {deep: true, immediate: false})
+}, {deep: true})
 </script>
 <template>
   <section class="mx-auto max-w-full px-0 py-8 sm:px-6 sm:py-12 lg:px-8">
@@ -31,16 +24,16 @@ watch(tag, (val) => {
     </section>
     <WidgetsTitle>Par filière <i class="text-carto-pink">{{ tag?.name }}</i></WidgetsTitle>
     <p class="text-carto-gray300 m-4">{{ tag?.description }}</p>
-    <ListTitle :count="items?.length"/>
-    <div v-if="items?.length > 0">
+    <ListTitle :count="data.count"/>
+    <div v-if="data.count > 0">
       <ul class="grid grid-cols-1 gap-y-4 lg:grid-cols-3 sm:gap-x-6 sm:gap-y-4 lg:gap-x-4 mt-4">
         <li class="group relative flex flex-col overflow-hidden rounded-lg border border-gray-200 bg-white45"
-            v-for="fiche in items" :key="fiche.id">
+            v-for="fiche in data.hits" :key="fiche.id">
           <ShopItem :fiche/>
         </li>
       </ul>
     </div>
-    <div class="prose lg:prose-xl" v-if="items?.length === 0">
+    <div class="prose lg:prose-xl" v-if="data.count === 0">
       Rien n'a été trouvé :-(
     </div>
   </section>
